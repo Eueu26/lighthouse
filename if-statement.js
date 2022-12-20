d7b5739fc352ed29b4dde6750e3c4634acc2f040
Eunice <117746227+Eueu26@users.noreply.github.com>
2022-12-20 13:30:59 -0500

    needed to add string length to the variable

--- if-statement.js
+++ if-statement.js
@@ -1,6 +1,7 @@
-const fruit = 'orange'
+let fruit = 'orange'
+fruit = fruit.length
 if (fruit > 5) {
-console.log('The fruit name has more than five characters.');
+  console.log('The fruit name has more than five characters.');
 } else {
   console.log('The fruit name has five characters or less.');
 }
